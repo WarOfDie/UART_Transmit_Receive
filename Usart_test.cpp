af53ea5bc81900148103a2b9f52cc8c1d504f476
WarOfDie <94315325+WarOfDie@users.noreply.github.com>
2024-07-20 12:06:11 +0300

    Add files via upload

---
+++ Usart_test.cpp
@@ -0,0 +1,37 @@
+/*
+ * Usart_test.cpp
+ *
+ *  Created on: 19 Tem 2024
+ *      Author: alper
+ */
+#include "main.h"
+#include "uart.h"
+
+UART_HandleTypedef_t Handle;
+
+
+void UARTConfig(void);
+
+int main(void)
+{
+	uint8_t data [] = { 0x66, 0x65, 0x72, 0x67, 0x61, 0x6E, 0x69}; // f: 0x66 e: 0x65 r: 0x72 g: 0x67 a: 0x61 n: 0x6E i: 0x69
+	UARTConfig();
+	UARTBase huart(&Handle);
+
+	huart.USART_Transmit(&Handle, data , 7);
+	huart.UART_Recieve(&Handle, data, 1);
+
+
+	return 0;
+}
+
+void UARTConfig(void)
+{
+	Handle.Instance = USART2;
+	Handle.Init.BaudRate = 9600;
+	Handle.Init.HardWareFlowControl = USART_HW_CTS_RTS;
+	Handle.Init.Mode = USART_MODE_RX_TX;
+	Handle.Init.Parity = USART_PARITY_OFF;
+	Handle.Init.StopBits = USART_STOPBITS_1;
+	Handle.Init.WordLength = USART_WORDLENGTH_8BITS;
+}
---
+++ uart.h
@@ -0,0 +1,85 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jul 19, 2024
+ *      Author: alper
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+#include "stm32f446xx.h"
+
+/*
+ * @def_group MODE_Types
+ */
+#define USART_MODE_TX      					((uint32_t)(0x00000008))
+#define USART_MODE_RX      					((uint32_t)(0x00000004))
+#define USART_MODE_RX_TX    				((uint32_t)(0x0000000C))
+/*
+ * @def_group WordLength_Types
+ */
+#define USART_WORDLENGTH_8BITS				((uint32_t)(0x00000000))
+#define USART_WORDLENGTH_9BITS				((uint32_t)(0x00001000))
+/*
+ * @def_group Parity_Types
+ */
+#define USART_PARITY_ON_EVEN				((uint32_t)(0x0000600))
+#define USART_PARITY_ON_ODD					((uint32_t)(0x0000400))
+#define USART_PARITY_OFF					((uint32_t)(0x0000000))
+/*
+ * @def_group StopBits_Types
+ */
+#define USART_STOPBITS_1					((uint32_t)(0x0000000))
+#define USART_STOPBITS_HALF					((uint32_t)(0x0001000))
+#define USART_STOPBITS_2					((uint32_t)(0x0002000))
+#define USART_STOPBITS_1_HALF				((uint32_t)(0x0003000))
+/*
+ * @def_group StopBits_Types HardWareFlowControl_Types
+ */
+
+#define USART_HW_NONE						((uint32_t)(0x0000000))
+#define USART_HW_CTS						((uint32_t)(0x0000200))
+#define USART_HW_RTS						((uint32_t)(0x0000100))
+#define USART_HW_CTS_RTS					((uint32_t)(0x0000300))
+
+#define __USART_DIV_VALUE_16(__CLK__,__BAUD__) (25*(__CLK__)/(4*(__BAUD__)))
+
+
+typedef enum
+{
+	USART_DISABLE = 0x00,
+	USART_ENABLE = !USART_DISABLE
+
+}USART_FunctionalState_t;
+
+using GPIODEF = \
+struct USART_InitTypedef_t {
+
+	uint32_t Mode;     					/*UART modlarını ayarlar @def_group MODE_Types mocroları ile ulaşılabilir*/
+	uint32_t BaudRate; 					/* UART Baud Rate (veri iletim hızı) ayarlaması yapılır.*/
+	uint32_t Parity;   					/*UART Parity bit ayarını yapar.*/
+	uint32_t StopBits; 					/*UART Stop bit ayarını yapar.*/
+	uint32_t WordLength;				/*UART Word Length (veri kelime uzunluğu) ayarını yapar.*/
+	uint32_t HardWareFlowControl;		/*UART Hardware Flow control (donanım akış kontrolü ) ayarını yapar*/
+
+};
+struct UART_HandleTypedef_t {
+
+	GPIODEF Init;
+	USART_TypeDef *Instance;
+
+};
+
+class UARTBase{
+public:
+	UARTBase(UART_HandleTypedef_t *USART_Handle);
+	void UART_Recieve(UART_HandleTypedef_t *USART_Handle,uint8_t *pData,uint16_t dataSize);
+	void USART_Transmit(UART_HandleTypedef_t *USART_Handle,uint8_t *pData,uint16_t dataSize);
+
+private:
+	void USART_PerphCmd(UART_HandleTypedef_t *USART_Handle,USART_FunctionalState_t State);
+	void init(UART_HandleTypedef_t *USART_Handle);
+};
+
+#endif /* INC_UART_H_ */
